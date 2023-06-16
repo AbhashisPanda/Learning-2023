e662268d5521ae361f6d666e42cf057157ab1e44
AbhashisPanda <136494451+AbhashisPanda@users.noreply.github.com>
2023-06-16 20:46:24 +0530

    Add files via upload

--- Module1/Day 1/Level 2/q1.c
+++ Module1/Day 1/Level 2/q1.c
@@ -1,1 +1,23 @@
-hghdjcv
+#include <stdio.h>
+int main() {
+     int a,b,c ;
+     printf("Enter three numbers\n");
+     scanf("%d,%d,%d\n",&a,&b,&c);
+    if (a > b) {
+		if (a > c) {
+			printf("Biggest number %d\n",a);
+		}
+		else {
+			printf("Biggest number %d\n",c);
+		}
+    }
+    else if(b > a) {
+        if (b > c) {
+			printf("Biggest number %d\n",b);
+		}
+		else {
+			printf("Biggest number %d\n",c);
+		}
+    }
+	return 0;
+}
