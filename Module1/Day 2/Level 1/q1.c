4af5ee54f7ac6f4b4acac28e3f2501d1d39688ad
AbhashisPanda <136494451+AbhashisPanda@users.noreply.github.com>
2023-06-16 22:16:50 +0530

    Add files via upload

--- Module1/Day 2/Level 1/q1.c
+++ Module1/Day 2/Level 1/q1.c
@@ -1,1 +1,14 @@
-D
+#include <stdio.h>
+
+int main() {
+  double x = 0.7;
+  unsigned long long int bits = *(unsigned long long int *)&x;
+  int exp = (bits >> 52) & 0x7FF;
+  printf("Exponent in Hex: 0x%X\n", exp);
+  printf("Exponent in Binary: 0b");
+  for (int i = 10; i >= 0; i--) {
+    printf("%d", (exp >> i) & 1);
+  }
+  printf("\n");
+  return 0;
+}
