8ea141088cc53a0188f62646a04d695b7c91b4e8
AbhashisPanda <136494451+AbhashisPanda@users.noreply.github.com>
2023-06-16 20:29:31 +0530

    Add files via upload

--- Module1/Day 1/Level 1/q1.c
+++ Module1/Day 1/Level 1/q1.c
@@ -1,1 +1,13 @@
-jfjg,jjuk
+#include <stdio.h>
+int main() {
+     int a,b ;
+     printf("Enter two numbers\n");
+     scanf("%d,%d\n",&a,&b);
+    if (a > b) {
+       printf("Biggest number %d\n",a);
+    }
+    else if(b > a) {
+        printf("Biggest number %d\n",b);
+    }
+	return 0;
+}
